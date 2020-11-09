c16a9d382b88d98511f3f33d1ac3dfe4d73c3383
Christopher Poile <cpoile@gmail.com>
2020-11-08 20:14:47 -0600

    concurrency in go example

---
+++ concurrency_in_go/ch03/cond.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Button struct {
+	Clicked *sync.Cond
+}
+
+func main() {
+	button := Button{Clicked: sync.NewCond(&sync.Mutex{})}
+
+	subscribe := func(c *sync.Cond, fn func()) {
+		var goroutineRunning sync.WaitGroup
+		goroutineRunning.Add(1)
+		go func() {
+			goroutineRunning.Done()
+			for {
+				c.L.Lock()
+				c.Wait()
+				c.L.Unlock()
+				fn()
+			}
+		}()
+		goroutineRunning.Wait()
+	}
+
+	var clickRegistered sync.WaitGroup
+	clickRegistered.Add(3)
+	subscribe(button.Clicked, func() {
+		fmt.Println("Maximizing the window.")
+		clickRegistered.Done()
+	})
+	subscribe(button.Clicked, func() {
+		fmt.Println("Displaying annoying dialog box.")
+		clickRegistered.Done()
+	})
+	subscribe(button.Clicked, func() {
+		fmt.Println("Mouse clicked.")
+		clickRegistered.Done()
+	})
+
+	button.Clicked.Broadcast()
+	clickRegistered.Wait()
+
+	clickRegistered.Add(3)
+	button.Clicked.Broadcast()
+	clickRegistered.Wait()
+
+}
