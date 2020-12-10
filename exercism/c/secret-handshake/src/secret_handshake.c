e1863d105a467db7bda592ee7c9e9538a142b459
Christopher Poile <cpoile@gmail.com>
2020-12-09 20:10:54 -0500

    handshake

--- exercism/c/secret-handshake/src/secret_handshake.c
+++ exercism/c/secret-handshake/src/secret_handshake.c
@@ -1,6 +1,20 @@
 #include "secret_handshake.h"
+#include <stdlib.h>
+
+const int MAX_ARR_LEN = 4;
+char* actions[] = {"wink", "double blink", "close your eyes", "jump"};
 
 const char** commands(size_t number)
 {
+    char** ret = malloc(sizeof(char*) * MAX_ARR_LEN);
+    ret[0] = NULL;
 
+    int p = 0;
+    for (int i = 0; i < 4; ++i) {
+        int idx = (number & 1 << 4) ? 4-1-i : i;
+        if (number & 1 << idx) {
+            ret[p++] = actions[idx];
+        }
+    }
+    return (const char**) ret;
 }
